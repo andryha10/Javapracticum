875976614fb3b9011c4bed2331f82e1baa29ca66
andryha10 <53227681+andryha10@users.noreply.github.com>
2021-12-05 15:37:31 +0200

    Add files via upload
    Cw1

---
+++ T1_5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+
+public class T1_5 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		for (int i = 0; i < 3; i++) {
+			System.out.println(args[i]);	
+		}
+		in.close();
+	}
+}
+
