f289b6f9363a1d0a46297fd04f28546dbb086e19
andryha10 <53227681+andryha10@users.noreply.github.com>
2021-12-09 10:15:02 +0200

    Add files via upload
    Kodovbetskyi Cw10

---
+++ Equation_Main.java
@@ -0,0 +1,6 @@
+public class Equation_Main {
+    public static void main(){
+        QuadraticEquation eq = new QuadraticEquation(1, -26, 120);
+        eq.getResult();
+    }
+}
---
+++ Task_7_11_Main.java
@@ -0,0 +1,7 @@
+public class Task_7_11_Main {
+    public static void main(){
+        Kostum kostum = new Kostum("Hlopok", 120);
+        kostum.getMaterial();
+        System.out.println("Cost: " + kostum.getCost());
+    }
+}
