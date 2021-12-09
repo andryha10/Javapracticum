5c87c8425ba196c19ab3001d2e02c9c7afea44dd
andryha10 <53227681+andryha10@users.noreply.github.com>
2021-12-09 10:11:00 +0200

    Add files via upload
    Kodovbetskyi Cw8

---
+++ Main.java
@@ -0,0 +1,13 @@
+package T4_0;
+import T3_1.T3_1_Main;
+import T4_0.T4_0_Main;
+import T4_1.T4_1_Main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Task_3_1_Main.main();
+        T4_0_Main.main();
+        //Task_4_1_Main.main();
+    }
+}
---
+++ T3_1main.java
@@ -0,0 +1,31 @@
+package T3_1;
+import java.util.ArrayList;
+import java.util.List;
+
+public class T3_1main {
+    public static void main(){
+        List<Fraction> list = new ArrayList<>();
+        list.add(new Fraction(4, 10));
+        list.add(new Fraction(3, 5));
+        list.add(new Fraction(2, 3));
+        list.add(new Fraction(4, 6));
+        for (Fraction frac: list
+             ) {
+            frac.Skorotiti();
+        }
+        Fraction res = new Fraction(1, 1);
+        for (Fraction frac: list
+             ) {
+            res = addFrac(res, frac);
+        }
+        res.setNumerator(res.getNumerator() - res.getDenominator());
+        System.out.println(res);
+    }
+
+    public static Fraction addFrac(Fraction fraction1, Fraction fraction2){
+        Fraction frac = new Fraction(0, 0);
+        frac.setDenominator(Util.least_common_multiple(fraction1.getDenominator(), fraction2.getDenominator()));
+        frac.setNumerator(fraction1.getNumerator()*(frac.getDenominator()/fraction1.getDenominator()) + fraction2.getNumerator()*(frac.getDenominator()/fraction2.getDenominator()));
+        return frac;
+    }
+}
